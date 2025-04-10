9511993aad1b148856f4b31994e0fc391a41b250
Rabea Tasnim <rabeatasnim@gmail.com>
2025-04-10 18:56:25 +0600

    Update Demo tui and functions.c

--- Demo tui and functions.c
+++ Demo tui and functions.c
@@ -5,20 +5,23 @@
 #include <time.h>
 #include <unistd.h> // For sleep()
 #include <windows.h>
-t
+
+
 void gotoxy(int x, int y) {
   COORD c;
   c.X = x;
   c.Y = y;
   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
+       }
+
  void slines(int a){
      printf("%c",201);
        for (int i = 0; i < a; i++) {
     printf("%c", 205);
- }
+   }
  printf("%c",187);
  }
+
 
   void mlines(int a){
      printf("%c",204);
@@ -27,7 +30,6 @@ void gotoxy(int x, int y) {
  }
  printf("%c",185);
  }
-
 
 
   void elines(int a){
@@ -49,56 +51,50 @@ void gotoxy(int x, int y) {
 
 
 int main(){
-    printf("\e[5;41mthis happends!!\e[0m\n");
-    printf("i am ok\n");
-    gotoxy(0,10);
-      printf("%c\n", 185);
-            printf("%c\n", 187);
-                  printf("%c\n\n", 201);
-                        printf("%c\n\n", 200);
-                                                printf("%c\n\n", 204);
-                                                                                                printf("%c\n\n", 205);
-                                                                                    printf("%c\n\n", 188);
-
-
-
-
+  printf("\e[5;41mthis happens!!\e[0m\n");
+  printf("i am ok\n");
+  gotoxy(0,10);
+  printf("%c\n", 185);
+  printf("%c\n", 187);
+  printf("%c\n\n", 201);
+  printf("%c\n\n", 200);
+  printf("%c\n\n", 204);
+  printf("%c\n\n", 205);
+  printf("%c\n\n", 188);
 
 
       char a[50]="icecream";
-      char b[50]="grocceries";
+      char b[50]="groceries";
       char c[50]="09-04-2025 17:02:54";
       int i=200;
       float j=3000;
       gotoxy(7,3);
       slines(98);
       gotoxy(7,4);
-                    printf("%c                                        chatagories: %-45s%c",186,b,186);
-                    gotoxy(7,5);
-                    mlines(98);
-                    gotoxy(7,6);
-                    printf("%-40c\e[1;31mNo items in this category.\e[0m%34c",186,186);
-                    gotoxy(7,7);
-                    mlines(98);
-                    gotoxy(7,8);
-                  printf("%c Name: %-14s | Quantity: %-8d | Price: %-11.2f | Added on: %-22s%C",186,a,i,j,c,186);
-                  gotoxy(7,9);
-                  dashes_vline(97);
-                  gotoxy(7,10);
-                  printf("%c\e[5;41m Name: %-14s | Quantity: %-8d | Price: %-11.2f | Added on: %-22s\e[0m%C",186,a,i,j,c,186);
-                 gotoxy(7,11);
-//                 dashes_vline(97);
-//                  gotoxy(7,8);
-                  elines(98);
+      printf("%c                                        categories: %-45s%c",186,b,186);
+      gotoxy(7,5);
+      mlines(98);
+      gotoxy(7,6);
+      printf("%-40c\e[1;31mNo items in this category.\e[0m%34c",186,186);
+      gotoxy(7,7);
+      mlines(98);
+      gotoxy(7,8);
+      printf("%c Name: %-14s | Quantity: %-8d | Price: %-11.2f | Added on: %-22s%C",186,a,i,j,c,186);
+      gotoxy(7,9);
+      dashes_vline(97);
+      gotoxy(7,10);
+      printf("%c\e[5;41m Name: %-14s | Quantity: %-8d | Price: %-11.2f | Added on: %-22s\e[0m%C",186,a,i,j,c,186);
+      gotoxy(7,11);
+//dashes_vline(97);
+// gotoxy(7,8);
+      elines(98);
 
-                  int num=6;
-                  for(int i=0;i<10;i++){
-                    num++;
-                  }
-                  printf("%d",num);
-
-
-
+      int num=6;
+     for(int i=0;i<10;i++){
+                              num++;
+                          }
+  
+      printf("%d",num);
 
     return 0;
 }
